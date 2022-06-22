1c0703247566b2c28e822d75b7e5efe47b802c60
ale <102003449+alethoo@users.noreply.github.com>
2022-06-22 16:48:42 -0600

    add html file server

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 let express = require('express');
 let app = express();
 
+absolutePath = __dirname + "/views/index.html"
+console.log(absolutePath);
+
+app.get("/", function(request, response) {
+    response.sendFile(absolutePath);
+});
+
 console.log("Hello World");
 
 
