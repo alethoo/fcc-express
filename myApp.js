2f785e673d9a43b0515733cc740f9264ef89e5da
ale <102003449+alethoo@users.noreply.github.com>
2022-06-22 20:40:07 -0600

    cleaned up logs

--- myApp.js
+++ myApp.js
@@ -3,44 +3,11 @@ let app = express();
 let htmlPath = __dirname + "/views/index.html";
 let middlewarePath = __dirname + "/public";
 
-
-
-console.log(htmlPath);
-
 app.get("/", function(request, response) {
     response.sendFile(htmlPath);
 });
 
 app.use("/public", express.static(middlewarePath));
-
-console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
